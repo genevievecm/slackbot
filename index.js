1770528360f16d18db91a4c5f409da2ecb38cb12
Genevieve Moreau <gmoreau@forgemedia.ca>
2016-07-20 17:34:25 -0400

    experimenting with postman

--- index.js
+++ index.js
@@ -18,32 +18,42 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //use 3000 if it isn’t set
 app.set('port', process.env.PORT || 3000);
 
-app.get('/', function(req, res){
-  res.send('It worxsdfsdfsdfdfdsfks!');
+// app.get('/', function(req, res){
+//   res.send('It worxsdfsdfsdfdfdsfks!');
+// });
+
+app.post('/post', function(req, res){
+
+  	var parsed_url = url.format({
+	    pathname: 'https://yoda.p.mashape.com/yoda',
+	    query: {
+	    	mashape_key: 'YKlfqMYuB3mshRHjFWJV8vaDW0zLp1ziHfsjsnZDUZK7YREDEX',
+      		sentence: 'hello its me'
+    	}
+  	});
+
+  	parsed_url = parsed_url.replace(/%20/g, "+");
+
+	request(parsed_url, function(error, response, body) {
+
+		res.send(body);
+
+		if(!error && res.statusCode == 200) {
+
+			//res.send('heelllooooo');
+		}
+	});
+
 });
-
-// app.post('/post', function(req, res){
-//   	var parsed_url = url.format({
-//     pathname: 'https://yoda.p.mashape.com/yoda',
-//     query: {
-//     	q: req.body.text
-//     }
-// });
-
-// request(parsed_url, function(error, res, body) {
-// 	if(!error && res.statusCode == 200) {
-// 		var data = JSON.parse(body);
-// 		var first_url = data.response.hits[0].result.url;
-
-// 		var body = {
-// 			response_type: "in_channel",
-// 			text: first_url
-// 		};
-
-// 		res.send(body);
-// 	}
-// });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+// These code snippets use an open-source library. http://unirest.io/nodejs
+// unirest.get("https://yoda.p.mashape.com/yoda?sentence=You+will+learn+how+to+speak+like+me+someday.++Oh+wait.")
+// .header("X-Mashape-Key", "YKlfqMYuB3mshRHjFWJV8vaDW0zLp1ziHfsjsnZDUZK7YREDEX")
+// .header("Accept", "text/plain")
+// .end(function (result) {
+//   console.log(result.status, result.headers, result.body);
+// });
